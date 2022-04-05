19ed337f711809ca299e2c9d628c09111fdcf2c2
Rahul Hazarika <101569226+RahulHazarika12@users.noreply.github.com>
2022-04-05 19:30:47 +0530

    Update sample-registration-form-validation.js

--- sample-registration-form-validation.js
+++ sample-registration-form-validation.js
@@ -79,17 +79,7 @@ function userid_validation(uid, mx, my) {
   return true;
 }
 
-function passid_validation(passid, mx, my) {
-  var passid_len = passid.value.length;
-  if (passid_len == 0 || passid_len >= my || passid_len < mx) {
-    alert(
-      "Password should not be empty / length be between " + mx + " to " + my
-    );
-    passid.focus();
-    return false;
-  }
-  return true;
-}
+
 function allLetter(uname) {
   var letters = /^[A-Za-z]+$/;
   if (uname.value.match(letters)) {
